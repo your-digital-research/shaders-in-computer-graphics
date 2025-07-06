c57be94c6811563e674651898fff8167aef11a87
Davit Badalyan <davit.badalyan.00@gmail.com>
2025-07-06 18:00:09 +0400

    CMakeLists initial configurations and welcoming message

---
+++ Project/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // NOTE - This is the main entry point of the application.
+
+    std::cout << "Welcome to \"Shaders In Computer Graphics\" ^_^" << std::endl;
+
+    return 0;
+}
