e166f0d64e6e52f1c5f351211050692d7b8a4155
Davit Badalyan <davit.badalyan.00@gmail.com>
2025-07-08 02:01:36 +0400

    Update CMakeLists and create blank window

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,3 +1,6 @@
+#define GL_SILENCE_DEPRECATION
+
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main()
@@ -5,6 +8,38 @@ int main()
     // NOTE - This is the main entry point of the application.
 
     std::cout << "Welcome to \"Shaders In Computer Graphics\" ^_^" << std::endl;
+
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW\n";
+
+        return -1;
+    }
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Shaders In Computer Graphics", nullptr, nullptr);
+
+    if (!window)
+    {
+        std::cerr << "Failed to create GLFW window\n";
+
+        glfwTerminate();
+
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        // Rendering code will go here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
